7fa4c32483e92c1d1b570395036a1df27636bb3c
Alexandre L <108535805+Newaluigi@users.noreply.github.com>
2023-06-29 10:32:04 +0200

    first commit

---
+++ lastCrusade.java
@@ -0,0 +1,13 @@
+class lastCrusade {
+    public static void main(String[] args) {
+        String titleMovie = "Indiana Jones and the Last Crusade";
+        Boolean hasSeen = true;
+        int year = 1989;
+        double imdbRating = 8.2;
+
+        System.out.println("Titre du film (VO) : " + titleMovie);
+        System.out.println("Déjà vu? : " + hasSeen);
+        System.out.println("Année de sortie : " + year);
+        System.out.println("Note IMDB : " + imdbRating);
+    }
+}
